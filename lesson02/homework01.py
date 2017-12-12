ef2820daefff29e9c6cde525ff13460db3eed760
wangwei96 <965158007@qq.com>
2017-12-12 17:09:17 +0800

    new homework and some improve

--- lesson02/homework01.py
+++ lesson02/homework01.py
@@ -1,10 +1,18 @@
-#质数是只能被1和它本身整出的数，编程找出100以内全部的质数。
-for i in range(1,101):
-    k=0
-    for j in range (1,i+1):
-        if i%j==0:
-            k+=1
-            continue
-    if k<3:
-        print(i)
-
+#质数是只能被1和它本身整出的数，编程
+#输入一个数字，求其以内全部的质数。
+def prime():
+    print('请输入合适的范围：')
+    num=int(input())
+    if num>=0:
+        for i in range(1,num+1):
+            k=0
+            for j in range (1,i+1):
+                if i%j==0:
+                    k+=1
+                    continue
+            if k<3:
+                print(i)
+    else:
+        print('请输入正确的数字')
+        prime()
+prime()
--- lesson02/homework02.py
+++ lesson02/homework02.py
@@ -2,14 +2,25 @@
    如下：1，1，2，3，5... 请通过编程得到数列的前100项，
    并输出99项与100项的比值。
 '''
-j=1
-k=0
-x=0
-print('斐波那契数第 1 位是',j)
-for i in range(1,100):
-    x=j
-    j+=k
-    k=x
-    print('斐波那契数第',i+1,'位是',j)
-    ratio=k/j
-print('斐波那契数第 99 项与第 100 项的比是',ratio)
+def Fibonacci():
+    print('请输入合适的范围，要求是大于0的数字：')
+    put=int(input())
+    j=1
+    k=0
+    x=0
+    if put==1:
+        print('斐波那契数第 1 位是',j)
+    elif put>1:
+        print('斐波那契数第 1 位是',j)
+        for i in range(1,put):
+            x=j
+            j+=k
+            k=x
+            print('斐波那契数第',i+1,'位是',j)
+            ratio=k/j
+        print('斐波那契数第',put-1,'项与第',put,'项的比是',ratio)
+    else :
+        print('请输入正确的值：')
+        Fibonacci()
+
+Fibonacci()
